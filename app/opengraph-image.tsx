8333a8512fea71f0efec672b542e2e1f989eab61
ʎɥdɹnꟽ <123694080+peepigod@users.noreply.github.com>
2024-11-30 01:13:29 -0500

    fix opengraph continental

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -12,7 +12,7 @@ export default async function Image() {
 
   return new ImageResponse(
     <div style={{ display: 'flex', justifyContent: 'center', background: '#000000', width: '100%', height: '100%' }}>
-      <img width={1200} height={580} style={{ objectFit: 'contain' }} src={`${process.env.NEXT_PUBLIC_BASE_URL}/ticket/afordin-sponsor.png`} alt={`Continental`} />
+      <img width={1200} height={580} style={{ objectFit: 'contain' }} src={`${process.env.NEXT_PUBLIC_BASE_URL}/afordin-sponsor.png`} alt={`Continental`} />
     </div>
     , { ...size })
 
